6f6a60c8bfab9e5fd9b6430ddecb12a95f7bc9dc
MC-AS-Huier <2133404320@qq.com>
2023-02-01 00:48:16 +0800

    初始化

---
+++ IdealChat/IdealChatPlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace IdealChat;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+public partial class IdealChatPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+}
